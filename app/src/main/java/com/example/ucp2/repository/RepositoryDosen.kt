0ff7490c3f2d8ce1bb72a3e05541bdf6866fc71e
azzamaflah <azzamhebat46@gmail.com>
2024-12-19 16:53:20 +0700

    mengahpus fun get dosen, delete dan update pada RepositoryDosen.kt

--- app/src/main/java/com/example/ucp2/repository/RepositoryDosen.kt
+++ app/src/main/java/com/example/ucp2/repository/RepositoryDosen.kt
@@ -4,9 +4,6 @@ import com.example.ucp2.data.entity.Dosen
 import kotlinx.coroutines.flow.Flow
 
 interface RepositoryDosen {
-    suspend fun inserDosen(dosen: Dosen)
+    suspend fun insertDosen(dosen: Dosen)
     fun getAllDosen(): Flow<List<Dosen>>
-    fun getDosen(nidn: String): Flow<Dosen>
-    suspend fun deleteDosen(dosen: Dosen)
-    suspend fun updateDosen(dosen: Dosen)
 }
