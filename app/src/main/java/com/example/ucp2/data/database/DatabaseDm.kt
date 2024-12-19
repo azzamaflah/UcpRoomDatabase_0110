ba97bb1888f0237681c1082645e7183c4fafdad7
azzamaflah <azzamhebat46@gmail.com>
2024-12-19 16:51:19 +0700

    rename database dan jadikan 1 database

--- app/src/main/java/com/example/ucp2/data/database/DatabaseDm.kt
+++ app/src/main/java/com/example/ucp2/data/database/DatabaseDm.kt
@@ -5,22 +5,24 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.ucp2.data.dao.DosenDao
+import com.example.ucp2.data.dao.MataKuliahDao
 import com.example.ucp2.data.entity.Dosen
 
 @Database(entities = [Dosen::class], version = 1, exportSchema = false)
-abstract class DosenDatabase : RoomDatabase(){
+abstract class DatabaseDm : RoomDatabase() {
 
     abstract fun dosenDao(): DosenDao
+    abstract fun matakuliahDao(): MataKuliahDao
 
-    companion object{
+    companion object {
         @Volatile
-        private var Instance: DosenDatabase? = null
+        private var Instance: DatabaseDm? = null
 
-        fun getDatabase(context: Context): DosenDatabase {
+        fun getDatabase(context: Context): DatabaseDm {
             return (Instance ?: synchronized(this) {
                 Room.databaseBuilder(
                     context,
-                    DosenDatabase::class.java,
+                    DatabaseDm::class.java,
                     "DosenDatabase"
 
                 )
