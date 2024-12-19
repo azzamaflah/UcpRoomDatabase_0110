c1e6bc83c9d5cdb0d9161232c9bbbb96f639144d
azzamaflah <azzamhebat46@gmail.com>
2024-12-20 05:40:15 +0700

    Menambahkan fungsi getDosen(nidn: String) untuk mengambil data dosen berdasarkan NIDN.

--- app/src/main/java/com/example/ucp2/repository/RepositoryDosen.kt
+++ app/src/main/java/com/example/ucp2/repository/RepositoryDosen.kt
@@ -6,4 +6,6 @@ import kotlinx.coroutines.flow.Flow
 interface RepositoryDosen {
     suspend fun insertDosen(dosen: Dosen)
     fun getAllDosen(): Flow<List<Dosen>>
+    fun getDosen(nidn: String): Flow<Dosen>
+
 }
