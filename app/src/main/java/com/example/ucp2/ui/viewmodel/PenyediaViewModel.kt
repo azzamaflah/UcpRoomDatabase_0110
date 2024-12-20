0b179c7b1c2b4aeac05b67d9b1ee0a929a1578ba
azzamaflah <azzamhebat46@gmail.com>
2024-12-21 01:35:54 +0700

    Menambahkan penyedia ViewModel untuk Dosen dan Mata Kuliah dengan initializer untuk semua ViewModel terkait

---
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,60 @@
+package com.example.ucp2.ui.viewmodel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.ucp2.RunApp
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        // Initializer untuk DosenViewModel
+        initializer {
+            DosenViewModel(
+                runApp().containerApp.repositoryDosen
+            )
+        }
+
+        // Initializer untuk HomeDosenViewModel
+        initializer {
+            HomeDosenViewModel(
+                runApp().containerApp.repositoryDosen
+            )
+        }
+
+        // Initializer untuk MataKuliahViewModel
+        initializer {
+            MataKuliahViewModel(
+                runApp().containerApp.repositoryMatakuliah
+            )
+        }
+
+        // Initializer untuk HomeMataKuliahViewModel
+        initializer {
+            HomeMataKuliahViewModel(
+                runApp().containerApp.repositoryMatakuliah
+            )
+        }
+
+        // Initializer untuk DetailMataKuliahViewModel
+        initializer {
+            DetailMatakuliahViewModel(
+                createSavedStateHandle(),
+                runApp().containerApp.repositoryMatakuliah
+            )
+        }
+
+        // Initializer untuk UpdateMataKuliahViewModel
+        initializer {
+            UpdateMataKuliahViewModel(
+                createSavedStateHandle(),
+                runApp().containerApp.repositoryMatakuliah
+            )
+        }
+    }
+}
+
+// Extension function untuk mengakses instance RunApp
+fun CreationExtras.runApp(): RunApp =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as RunApp)
