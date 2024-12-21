1cb0a77b9f04854a0334cc3752ef6932f7f3cf4c
azzamaflah <azzamhebat46@gmail.com>
2024-12-21 15:09:07 +0700

    menambahkan rute untuk update matkul

--- app/src/main/java/com/example/ucp2/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/ucp2/ui/navigation/AlamatNavigasi.kt
@@ -3,32 +3,49 @@ package com.example.ucp2.ui.navigation
 interface AlamatNavigasi {
     val route: String
 
-    // Rute untuk Menu
+    // Rute untuk Menu Utama
     object DestinasiMenu : AlamatNavigasi {
         override val route = "menu"
     }
 
-    // Rute untuk Dosen
-    object DestinasiDosen : AlamatNavigasi {
-        override val route = "dosen"
+    // Rute untuk Home Dosen
+    object DestinasiHomeDosen : AlamatNavigasi {
+        override val route = "home_dosen"
     }
 
-    // Rute untuk Mata Kuliah
-    object DestinasiMataKuliah : AlamatNavigasi {
-        override val route = "mata_kuliah"
+    // Rute untuk Insert Dosen
+    object DestinasiInsertDosen : AlamatNavigasi {
+        override val route = "insert_dosen"
     }
 
-    // Rute untuk detail dosen, menggunakan NIDN sebagai argumen
+    // Rute untuk Home Mata Kuliah
+    object DestinasiHomeMatakuliah : AlamatNavigasi {
+        override val route = "home_matakuliah"
+    }
+
+    // Rute untuk Insert Mata Kuliah
+    object DestinasiInsertMatakuliah : AlamatNavigasi {
+        override val route = "insert_matakuliah"
+    }
+
+    // Rute untuk Detail Dosen (menggunakan NIDN sebagai argumen)
     object DestinasiDetailDosen : AlamatNavigasi {
         override val route = "detail_dosen"
         const val NIDN = "nidn"
         val routesWithArg = "$route/{$NIDN}"
     }
 
-    // Rute untuk detail mata kuliah, menggunakan Kode sebagai argumen
+    // Rute untuk Detail Mata Kuliah (menggunakan Kode sebagai argumen)
     object DestinasiDetailMataKuliah : AlamatNavigasi {
-        override val route = "detail_mata_kuliah"
+        override val route = "detail_matakuliah"
         const val KODE = "kode"
         val routesWithArg = "$route/{$KODE}"
     }
-}
+
+        // Rute untuk Update Mata Kuliah (menggunakan Kode sebagai argumen)
+        object DestinasiUpdateMatakuliah : AlamatNavigasi {
+            override val route = "update_matakuliah"
+            const val KODE = "kode"
+            val routesWithArg = "$route/{$KODE}"
+        }
+    }
